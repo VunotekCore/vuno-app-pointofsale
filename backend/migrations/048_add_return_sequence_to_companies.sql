f9579e4d44b6acb1d54befcba25b0312fa887b2d
Dail Flores <dflores2t@gmail.com>
2026-04-12 19:35:09 -0600

    Revert: remove migration file (not pushed to github)

--- backend/migrations/048_add_return_sequence_to_companies.sql
+++
@@ -1,4 +0,0 @@
--- Migration: 048_add_return_sequence_to_companies
--- Description: Add return_sequence column to companies table for returns management
-
-ALTER TABLE companies ADD COLUMN return_sequence INT DEFAULT '1' AFTER invoice_sequence;
